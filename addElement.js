4b58ef8448f16779d3ab8eaa0fb3a9efe83f787d
horopciuc <horopciuc.florin@yahoo.ro>
2019-01-15 14:09:57 +0200

    add li into ul
    generate divs, add li to ul, delete them with different actions depending on the container

--- addElement.js
+++ addElement.js
@@ -3,7 +3,6 @@ var Module = (function() {
 	var // selectors & variable
 		$nOfInputs = $('#nmbInputs'),
 		$createBtn = $('#createBtn'),
-		$divId 	   = 1,
 		$elementId = 1,
 		$childsId  = 1;
 
@@ -17,49 +16,44 @@ var Module = (function() {
 			else {
 				createDivs(numberOfInputs);
 			}
+			removeLi();
 		})
 	}
 
+	// after we get the input
+	// we create a div to which we assign a class & a data-attribute
+	// and inside this div we add
+	// an input(w/ dataAtrb), a button(w/ class&dataAtrb) and an ul(w/ class&dataAtrb)
 	var createDivs = function(nOfDivs) {
 		for (var numberOfDivs = 0; numberOfDivs < nOfDivs; numberOfDivs++) {
-			// after we get the input
-			// we create a div to which we assign a class & a data-attribute
-			// and inside this div we add
-			// an input(w/ dataAtrb), a button(w/ class&dataAtrb) and an ul(w/ class&dataAtrb)
-			var strr = '<div class="divClass" id=' + $divId + ' style="float: left;"> '+
-					       '<input type="text" class="inputClass" data-inputId=' + $elementId + '>' +
+			var strr = '<div class="divClass" id="div-' + $elementId + '" style="float: left;"> '+
+					       '<input type="text" class="inputClass" id="input-' + $elementId + '">' +
 					       '<button class="btnClass" id=' + $elementId + ' >Save</button>' +
-					       '<ul class="ulClass" data-ulId=' + $elementId + '></ul>' +
+					       '<ul class="ulClass" id="container-' + $elementId + '"></ul>' +
 						'</div>';
 			$elementId++;
-			$divId++;
 			$('.mainDiv').append(strr);
 		}
-		removeLi();
 	}
 
+	// through the static already existing parent,
+	// we acees the dynamically created element (through it's class, ".btnClass")
+	// get the value of the input, verifying if it's empty, then if not,
+	// we create the element which will be appended to the sibling of the static parent,
+	// in this case, we're reffering to the ".ulClass"
 	var createLi = function() {
-
-		// through the static already existing parent,
-		// we acees the dynamically created element (through it's class, ".btnClass")
-		// get the value of the input, verifying if it's empty, then if not,
-		// we create the element which will be appended to the sibling of the static parent,
-		// in this case, we're reffering to the ".ulClass"
 		$('.mainDiv').on("click", ".btnClass", function() {
-			var inputValue = $(this).siblings('.inputClass').val();
-			var index = $(this).data('btnid');
-
-			var selectedValue = $("#input-"+index).val();
-
-			if ($.trim(inputValue) == "") {
+			var indexVal = 1*($(this).attr("id"));
+			var inputVal = $('#input-' + indexVal).val();
+			if ($.trim(inputVal) == "") {
 				console.log("orice inafara de asta");
 			}
 			else {
-				var liToBeAdded = '<li class="liClass" data-liId=' + $childsId + '>' + inputValue +
-								  	'<button class="delBtnClass" id=del' + $childsId + '>Delete</button'+
+				var liToBeAdded = '<li class="liClass" id="li-' + $childsId + '">' + inputVal +
+								  	'<button class="delBtnClass" id="asd' + $childsId + '">Delete</button'+
 								  '</li>';
 				$childsId++;
-				$(this).siblings('.ulClass').append(liToBeAdded);
+				$('#container-' +indexVal).append(liToBeAdded);
 			}
 		})
 	}
@@ -68,16 +62,26 @@ var Module = (function() {
 	// we delete the parent of the respective button,
 	// which is the li element containing him
 	var removeLi = function() {
-
-		// $('body').on("click", ".delBtnClass", function() {
-		// 	$(this).parent().remove();
-		// })
-		$('.btnClass').each(function() {
-			var index = $(this).attr("id");
-			$("#del"+index).on("click", function() {
-				console.log("clicked save");
+			$('.btnClass').each(function() {
+				var index = parseInt($(this).attr("id"));
+				$('#container-' + index).on("click", ".delBtnClass", function() {
+					switch(index) {
+						case 1:
+							$(this).parent().remove();
+							console.log("pe case 1");
+							break;
+						case 3:
+							$(this).parent().remove();
+							console.log('removed element with container ' +index);
+							console.log("pe case 3");
+							break;
+						default:
+							$(this).parent().remove();
+							console.log("pe case default");
+							break;
+					}
+				});
 			})
-		})
 	}
 
 	var	init = function() {
